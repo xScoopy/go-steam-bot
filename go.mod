791761ceeeb9d62a7be3019cc6fe5fdfab49867c
xScoopy <69348168+xScoopy@users.noreply.github.com>
2022-02-28 22:48:03 -0600

    able to post a message to private slack channel with bot

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module steamBot
 
 go 1.17
+
+require (
+	github.com/joho/godotenv v1.4.0
+	github.com/slack-go/slack v0.10.2
+)
+
+require github.com/gorilla/websocket v1.4.2 // indirect
--- steamBot.go
+++ steamBot.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
 )
 
+func getEnvVariable(key string) string {
+	//method to retrieve slackbot token from .env
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Problem loading .env file.")
+	}
+	return os.Getenv(key)
+}
+
 func main() {
-	fmt.Print("Hello World")
+	//initialize slack api connection 
+	api := slack.New(getEnvVariable("GOSECRET"))
+
+	channelID, timestamp, err := api.PostMessage(
+		"C034MB8U5M5",
+		slack.MsgOptionText("Hello World", false),
+	)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+	}
+	fmt.Printf("Message sent successfully to channel %s at %s", channelID, timestamp)
+
 }
